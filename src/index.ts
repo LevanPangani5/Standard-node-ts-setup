c9d3a253424d0728bfe9857925778211178c0584
LevanPangani5 <levanfangani55@gmail.com>
2024-02-27 13:53:58 +0400

    added prettier pre-commit hook

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,27 @@
-import { createServer, IncomingMessage, ServerResponse } from "http";
-import { hello } from "./helper";
-hello();
+import {
+  createServer,
+  IncomingMessage,
+  ServerResponse
+} from 'http'
+import { hello } from './helper'
+hello()
+const x = 5
+
+console.log(x)
 export const server = createServer(
   (req: IncomingMessage, res: ServerResponse) => {
-    res.writeHead(200, { "Content-type": "application-json" });
+    res.writeHead(200, {
+      'Content-type': 'application-json'
+    })
     res.write(
       JSON.stringify({
-        data: "Hello",
-      }),
-    );
-    res.end();
-  },
-);
+        data: 'Hello0'
+      })
+    )
+    res.end()
+  }
+)
 
 server.listen(3000, () => {
-  console.log(`listening to port:3000`);
-});
+  console.log(`listening to port:3000`)
+})
--- src/tests/index.spec.ts
+++ src/tests/index.spec.ts
@@ -1,20 +1,20 @@
-import supertest from "supertest";
-import { server } from "../index";
+import supertest from 'supertest'
+import { server } from '../index'
 
-describe("server", () => {
-  const request = supertest.agent(server);
+describe('server', () => {
+  const request = supertest.agent(server)
 
   afterAll(() => {
-    server.close();
-  });
+    server.close()
+  })
 
-  it("should get /", async () => {
-    const res = await request.get("/");
+  it('should get /', async () => {
+    const res = await request.get('/')
 
-    expect(res.status).toBe(200);
-    console.log("ccccccccc", res.text);
+    expect(res.status).toBe(200)
+    console.log('ccccccccc', res.text)
     expect(JSON.parse(res.text)).toEqual({
-      data: "Hello",
-    });
-  });
-});
+      data: 'Hello'
+    })
+  })
+})
